16b7b91b2c88178fb16c45b158c78c9e2d059d6b
Marcin.Rozycki <marcin.rozycki@asseco.pl>
2020-06-28 15:52:58 +0200

    Initialize project from https://start.spring.io/

---
+++ src/main/java/pl/com/rozyccy/javadesignpatterns/examples/ExamplesApplication.java
@@ -0,0 +1,13 @@
+package pl.com.rozyccy.javadesignpatterns.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplesApplication.class, args);
+	}
+
+}
